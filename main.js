73371e007f882ac4f2490a64c227c45b85b54619
Oliver Thomas <63861649+oliverthomas93@users.noreply.github.com>
2020-08-17 10:32:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+/*let proPic = document.getElementById('profile-pic');
+let heroL = document.querySelector('.hero-left');
+let heroL2 = document.querySelector('.hero-left2');
+let cross = document.querySelector('.hero-left2 img');
+
+let switchInfoAbout = () => {
+    heroL.style.display = 'none';
+    heroL2.style.display = 'block';
+}
+let switchInfoClose = () => {
+    heroL.style.display = 'block';
+    heroL2.style.display = 'none';
+}
+
+proPic.addEventListener('click', switchInfoAbout);
+cross.addEventListener('click', switchInfoClose);*/
